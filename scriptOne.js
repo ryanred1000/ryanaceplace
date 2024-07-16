387c38416602bf0e611c792f0ed8a28cd3ae7233
ryanred1000 <ryanace10@hotmail.com>
2024-07-16 14:43:00 -0400

    added a bar between the about me title and text

--- scriptOne.js
+++ scriptOne.js
@@ -18,9 +18,15 @@ function makeVisible() {
     var divs = document.getElementsByClassName('content');
     for(let i = 0; i < divs.length; i++) {
         if(divs[i].id !== 'topBar') {
-            divs[i].style.display = "block";
+            divs[i].style.display = "flex";
+            for (let j = 0; j < divs[i].children.length; j++) {
+                divs[i].children[j].style.display = "flex";
+            }            
             setTimeout(function() {
                 divs[i].style.opacity = "1";
+                for (let j = 0; j < divs[i].children.length; j++) {
+                    divs[i].children[j].style.opacity = 1;
+                }                
             }, 100 + i * 1000);
         }
     }   
